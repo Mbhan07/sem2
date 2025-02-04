e34f4a471b0779a076996f5ed5d99e7b117a8a9c
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2025-02-04 09:02:00 -0800

    Created main.cpp, finally figured out how to use github omg!!

---
+++ hashTable/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+/*************************************************************************************
+This is the hash table project, which is yet another pesky continuance of Student List. 
+Concepts of interests:
+ - Hash Tables
+ Mahika Bhan, 02/01/2025
+**************************************************************************************/
+using namespace std;
+
+int main(){
+
+  return 0;
+}
