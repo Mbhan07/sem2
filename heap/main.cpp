15dd1c935dac5b228f018aa63a3bcabae94a335f
Mahika Bhan <bhan.mahika813@gmail.com>
2025-02-11 21:30:23 -0800

    mapped out functions

--- heap/main.cpp
+++ heap/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <iomanip>
+#include <string>
+
+
 /*
 
   This is heap
@@ -10,38 +14,68 @@
 */
 using namespace std;
 
+
+//probably need an add, remove, print and heap function
+
+void add();
+void print();
+int remove();
+void heapStuff();
+
+
+
+
+
 int main(){
 
-  //create heao
-  cout << "Would you like to enter numbers MANUAL or through a FILE? ";
+  //create heap
+  int heap[10];
 
-  char input[20];
+  int size = 0;
 
-  cin >> input;
+  //set everything equal to 0
+  for (int i = 1; i < 101; i++){
+    heap[i] = 0;
+  }
 
-  if(strcmp(input, "MANUAL") == 0){
-    //call manual funcgtion
-  }else if(strcmp(input, "FILE") == 0){
-    cout << "Enter the name of the file you would like to read: ";
-    char fileName[30];
-    cin >> fileName;
-    //call file function via fstream
-    //this function will read the file then call the function that manual will use 
+  while (true){
+    
+    cout << "Would you like to enter numbers MANUAL or through a FILE? ";
+
+    char input[20];
+
+    cin >> input;
+
+    if(strcmp(input, "MANUAL") == 0){
+      //call manual funcgtion
+    }else if(strcmp(input, "FILE") == 0){
+      cout << "Enter the name of the file you would like to read: ";
+      char fileName[30];
+      cin >> fileName;
+      //call file function via fstream
+    }
+
   }
   
   return 0;
 }
+void add(int * heap, int &size, int number){
+  heap[size] = number;
+  size++;
+  heapStuff(heap, size - 1);
+}
 
-void removeALL(){
+void print(){
 
 }
 
-void processFILE(){
+int remove(){
 
-  //call heapStuff once file processed
-  
 }
 
-void heapStuff(){
 
+//to sort the heap
+void heapStuff(int * heap, int index){
+  int parent = (index-1)/2;
+  ///what elese goes here....
 }
