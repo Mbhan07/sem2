8de9e3b9c3ea391ce050e004100924cf79d0d1a9
Mahika Bhan <bhan.mahika813@gmail.com>
2025-02-13 12:42:52 -0800

    come back to remove function

--- heap/main.cpp
+++ heap/main.cpp
@@ -29,7 +29,7 @@ void heapStuff(int * heap, int index);
 int main(){
 
   //create heap
-  int heap[10];
+  int heap[101];
 
   int size = 0;
 
@@ -39,21 +39,28 @@ int main(){
   }
 
   while (true){
+
+    int index = 0;
     
-    cout << "Would you like to enter numbers MANUAL or through a FILE? ";
+    
+    cout << "Would you like to enter numbers MANUAL, through a FILE, or QUIT? ";
 
     char input[20];
 
     cin >> input;
 
     if(strcmp(input, "MANUAL") == 0){
-      //call manual funcgtion
+      cout << "manual" << endl;
     }else if(strcmp(input, "FILE") == 0){
       cout << "Enter the name of the file you would like to read: ";
       char fileName[30];
       cin >> fileName;
       //call file function via fstream
+    }else if(strcmp(input, "QUIT") == 0){
+      exit(0);
     }
+
+    //will also need to delete largest or all nodes
 
   }
   
@@ -83,6 +90,28 @@ void print(int pos, int depth, int size){
 
 int remove(int * heap, int index){
   int temporary = heap[0];
+
+  int sizes = 0;
+
+  //get size of heap
+
+  while (heap[sizes] != 0){
+    sizes++;
+  }
+
+  sizes = sizes - 1;
+  int rightNode = index;
+
+  heap[index] = heap[sizes];
+
+  heap[sizes] = 0;
+
+  //if current node smaller than right/left
+  while(true){
+    cout << "something should occur here" << endl;
+  }
+
+  return temps;
   
 }
 
