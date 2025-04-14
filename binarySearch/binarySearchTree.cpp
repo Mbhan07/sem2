b24d6ed596c0bbc7ccddbb367eaf055396eebdae
Mahika Bhan <bhan.mahika813@gmail.com>
2025-04-13 22:08:28 -0700

    mapping

--- binarySearch/binarySearchTree.cpp
+++ binarySearch/binarySearchTree.cpp
@@ -12,21 +12,103 @@ struct binaryTree{
 using namespace std;
 
 int main(){
-  char input[100];
 
-  cout << "Would you like to read in numbers MANUALLY (1-999, separated by a single space) or through a FILE?" << endl;
-  cin >> input;
+  //make a new tree + set root node = -1
+  binaryTree * newTree = new binaryTree();
+  newTree -> data = -1;
 
-  if(strcmp(input, MANUALLY) == 0){
-    cout << "manualy" << endl;
-  }else if(strcmp(input, FILE) == 0){
-    char fileName[30];
-    cout << "what is the name of the file you want to use? make sure you include the file type as well (e.g. .txt)" << endl;
-    
-    
-  }else{
-    cout << "Not a valid input. Please enter MANUALLY or FILE in that exact format." << endl;
+  while (true){
+    char input[100];
+
+    cout << "Would you like to read in numbers MANUALLY (1-999, separated by a single space), through a FILE, PRINT, SEARCH, DELETE, or QUIT?" << endl;
+    cin >> input;
+
+    if(strcmp(input, "MANUALLY") == 0){
+
+      cout << "manualy" << endl;
+
+      int numbers;
+
+      cout << "How many numbers would you like to add? ";
+      cin >> numbers;
+
+      for(int i = 0; i < numbers; i++){
+
+	int numInput;
+
+	cout << "Please enter a number: ";
+	cin >> numInput;
+
+	//call add function
+	//then print function
+
+	cout << endl;
+	cout << endl;
+      }
+      
+    }else if(strcmp(input, "FILE") == 0){
+
+      char fileName[30];
+
+      cout << "what is the name of the file you want to use? make sure you include the file type as well (e.g. .txt)" << endl;
+
+      cin >> fileName;
+      cin.ignore();
+
+      ifstream file;
+
+      file.open(fileName);
+
+      int input = 0;
+
+      //read numbers in and add
+
+      while(file >> input){
+	//call add function
+      }
+      file.close();
+      //print function
+      
+    }else if(strcmp(input, "PRINT") == 0){
+
+      cout << "print" << endl;
+
+      //call print function
+
+    }else if(strcmp(input, "SEARCH")== 0){
+
+      cout << "search" << endl;
+
+      int value;
+
+      cout << "What number would you like to search for? " << endl;
+
+      cin >> value;
+
+      if(search(newTree, value) == true){
+
+	cout << "Number found: " << value << endl;
+
+      }else {
+	cout << "Number not found: " << value << endl;
+      }
+    }else if(strcmp(input, "DELETE") == 0){
+      int toDelete;
+      cout << "What value would you like to delete? ";
+      cin >> toDelete;
+      //call delete function
+    }else if(strcmp(input, "QUIT") == 0){
+      exit(0);
+    }else{
+      cout << "Not a valid input. Please enter MANUALLY or FILE in that exact format." << endl;
+      cin >> input;
+    }
   }
   
   return 0;
+}
+
+
+bool search( binaryTree * newTree, int numToSearchFor){
+
 }
