df46ac02ffc485714d99ad53cae6529578f3db12
Mahika Bhan <bhan.mahika813@gmail.com>
2025-06-01 18:29:52 -0700

    ehnrhnmhen

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <climits> //allows
 /*
 
   This is graph creator by Mahika Bhan, due May 30th, 2025. Graph Creator is a project used to visualize graphs and calculate the shortest distance/path using Djikstra's Algorithim. This program can add a vertex, add an edge, remove a vertex or edge, and find the shortest path.
@@ -20,7 +21,7 @@ void deleteEdge(char starting, char ending, int weight, int & vertexNum, int adj
 void addEdge(char starting, char ending, int weight, int &vertexNum,  int adjacentMatrix[20][20], char vertices[20]);
 void addVertex(char label, int & vertexNum, int adjacentMatrix[20][20], char vertices[20]);
 int getIndex(char index, int & vertexNum, int adjacentMattrix[20][20], char vertices[20]);
-
+void shortestPath(char starting, char ending, int vertexNum, int adjacentMatrix[20][20], char vertices[20]);
 
 
 int main(){
@@ -29,39 +30,93 @@ int main(){
   int adjacentMatrix[20][20] = {0};
   char vertices[20];
 
+  char input[25];
+
   while(true){
 
-    char input[20];
+
     cout << "What would you like to do? ADDEDGE, ADDVERTEX, REMOVEVERTEX, REMOVEEDGE, PRINTGRAPH, FINDSHORTEST, or QUIT." << endl;
     cin >> input;
 
     if(strcmp(input, "ADDVERTEX") == 0){
+
       char label[16];
+
       cout << "Vertex label? ";
+
       cin >> label;
+
       addVertex(label, vertexNum, adjacentMatrix, vertices);
-      
+
+      printMatrix(vertexNum, adjacentMatrix, vertices);
     }else if(strcmp(input, "ADDEDGE") == 0){
+      char startPoint;
+      char endPoint;
+      int edgeLength;
 
+      cout << "Start vertex? ";
+      cin >> startPoint;
+
+      cout << "End vertex? ";
+      cin >> endPoint;
+
+      cout << "Edge length? ";
+      cin >> edgeLength;
+
+      addEdge(startPoint, endPoint, edgeLength, vertexNum, adjacentMatrix, vertices);
+
+      printMatrix(vertexNum, adjacentMatrix, vertices);
+      
     }else if(strcmp(input, "REMOVEVERTEX") == 0){
+      char label;
+      cout << "Which vertex to delete? ";
 
+      cin >> label;
+
+      deleteVertex(label, vertexNum, adjacentMatrix, vertices);
+
+      printMatrix(vertexNum, adjacentMatrix, vertices);
+      
     }else if(strcmp(input, "REMOVEEDGE") == 0){
+      char start;
+      char end;
+
+      cout << "Start vertex? ";
+      cin >> start;
+
+      cout << "End vertex? ";
+      cin >> end;
+
+      removeEdge(start, end, vertexNum, adjacentMatrix, vertices);
+
+      printMatrix(vertexNum, adjacentMatrix, vertices);
 
     }else if(strcmp(input, "PRINTGRAPH") == 0){
 
+      printMatrix(vertexNum, adjacentMatrix, vertices);
+
     }else if(strcmp(input, "FINDSHORTEST") == 0){
+      char starting;
+      char ending;
 
+      cout << "Enter starting point for algorithim: ";
+      cin >> starting;
+
+      cout << "Enter ending point for algorithim: ";
+      cin >> ending;
+
+      shortestPath(starting, ending, vertexNum, adjacentMatrix, vertices);
+      
     }else if(strcmp(input, "QUIT") == 0){
-
+      cout << "Goodbye." << endl;
+      exit(0);
     }else{
       cout << "Invalid input. Please follow all syntax and pick: ADDEDGE, ADDVERTEX, REMOVEVERTEX, REMOVEEDGE, PRINTGRAPH, FINDSHORTEST, or QUIT." << endl;
+      cin >> input;
     }
-
-  
-
   }
   
-  addVertex('A', vertexNum, adjacentMatrix, vertices);
+  /*addVertex('A', vertexNum, adjacentMatrix, vertices);
   addVertex('B', vertexNum, adjacentMatrix, vertices);
   addVertex('C', vertexNum, adjacentMatrix, vertices);
   addVertex('D', vertexNum, adjacentMatrix, vertices);
@@ -76,17 +131,72 @@ int main(){
 
   deleteEdge('B', 'C', 2, vertexNum, adjacentMatrix, vertices);
 
-  printMatrix(vertexNum, adjacentMatrix, vertices);
-  
- 
+  printMatrix(vertexNum, adjacentMatrix, vertices);*/
+   
   return 0;
 }
 
+void shortestPath(char starting, char ending, int vertexNum, int adjacentMatrix[20][20], char vertices[20]){
+  const int biggie = INT_MAX;
+  int distance[20];
+  bool visited[20] = {false};
+
+  int startIndex = -1;
+  int endIndex = -1;
+
+  for(int i = 0; i < vertexNum; i++){
+    if(vertices[i]) == start){
+       startIndex = i;
+    }
+    if(vertices[i] == end){
+      endIndex = i;
+    }
+
+  }
 
 
+ if(startIndex == -1 || endIndex == -1){
+   cout << "Vertex label not valid. " << endl;
+   return;
+ }
 
+ for(int i = 0; i < vertexNum; i++){
+   dist[i] = biggie;
+ }
 
+dist[startIndex];
 
+for(int count = 0; count < vertexNum -1; count++){
+  int u = -1;
+  int minimumDistance = biggie;
+
+  for(int i = 0; i < vertexNum; i++){
+    if(!visited[i] && dist[i] < minimumDistance) {
+      minimumDistance = dist[i];
+      u = i;
+    }
+  }
+  if(u == -1){
+    break;
+  }
+
+  visited[u] = true;
+  for (int v = 0; v < vertexNum; v++){
+    if(adjacentMatrix[u][v] > 0 && !visited[v]){
+      if(dist[u] + adjacentMatrix[u][v] < dist[v]){
+	dist[v] = dist[u] + adjacentMatrix[u][v];
+      }
+    }
+  }
+
+}
+if(dist[endIndex] == biggie){
+  cout << "No path from " << start << " to " << end << endl;
+ }else {
+  cout << "Shortest distance from " << start << " to " << end << " is " << dist[endIndex] << endl;
+ }
+
+}
 void printMatrix(int & vertexNum, int adjacentMatrix[20][20], char vertices[20]){
   cout << "Here is the table: " << endl;
 
